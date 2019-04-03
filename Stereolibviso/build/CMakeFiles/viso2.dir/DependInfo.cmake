27c4f6a9728c4b909a57e562823668327547f706
Chen Bing <1252242281@qq.com>
2019-04-03 11:37:48 +0800

    Add files via upload

---
+++ Stereolibviso/build/CMakeFiles/viso2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/Michael/libviso2/src/demo.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/demo.cpp.o"
+  "/home/Michael/libviso2/src/filter.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/filter.cpp.o"
+  "/home/Michael/libviso2/src/matcher.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/matcher.cpp.o"
+  "/home/Michael/libviso2/src/matrix.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/matrix.cpp.o"
+  "/home/Michael/libviso2/src/reconstruction.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/reconstruction.cpp.o"
+  "/home/Michael/libviso2/src/triangle.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/triangle.cpp.o"
+  "/home/Michael/libviso2/src/viso.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/viso.cpp.o"
+  "/home/Michael/libviso2/src/viso_mono.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/viso_mono.cpp.o"
+  "/home/Michael/libviso2/src/viso_stereo.cpp" "/home/Michael/libviso2/build/CMakeFiles/viso2.dir/src/viso_stereo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Stereolibviso/build/CMakeFiles/viso2.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/viso2.dir/src/triangle.cpp.o"
+  "CMakeFiles/viso2.dir/src/reconstruction.cpp.o"
+  "CMakeFiles/viso2.dir/src/viso_stereo.cpp.o"
+  "CMakeFiles/viso2.dir/src/demo.cpp.o"
+  "CMakeFiles/viso2.dir/src/viso_mono.cpp.o"
+  "CMakeFiles/viso2.dir/src/viso.cpp.o"
+  "CMakeFiles/viso2.dir/src/filter.cpp.o"
+  "CMakeFiles/viso2.dir/src/matrix.cpp.o"
+  "CMakeFiles/viso2.dir/src/matcher.cpp.o"
+  "viso2.pdb"
+  "viso2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/viso2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
